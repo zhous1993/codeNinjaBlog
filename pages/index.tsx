8b9aaa001ed73ada41dfba7ee9099ddd26b311fc
DESKTOP-ER2OAAD\zs_lq <zhous@ai-cloud.edu>
2023-04-13 18:02:31 +0800

    fix : 每日诗歌改用getSserverSideProps

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@
  * @Author: DESKTOP-16EDV1I\zs_lq zhous0310@gmail.com
  * @Date: 2023-04-08 13:46:36
  * @LastEditors: DESKTOP-ER2OAAD\zs_lq zhous@ai-cloud.edu
- * @LastEditTime: 2023-04-13 16:35:16
+ * @LastEditTime: 2023-04-13 18:01:16
  * @FilePath: \Study\ninja-blog\pages\index.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -18,9 +18,8 @@ const blogList: Array<BlogItemType> = [
   { title: 'day03 contextMenu', id: '03', date: '2023-4-10', link: '/post/context-menu' },
   { title: 'day04 音频可视化', id: '04', date: '2023-4-10', link: '/post/music-player' },
 ];
-export async function getStaticProps() {
+export async function getServerSideProps() {
   let dailyPoetry = await getDailyPoetry();
-  console.log('今日诗歌：', JSON.parse(dailyPoetry));
   dailyPoetry = JSON.parse(dailyPoetry);
   return {
     props: {
