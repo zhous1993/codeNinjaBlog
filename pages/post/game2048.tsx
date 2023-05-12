0b2a1d57e7f2e712c11b1813aebf731fbe9e5b5b
DESKTOP-ER2OAAD\zs_lq <zhous@ai-cloud.edu>
2023-05-12 10:47:14 +0800

    feat: 完善2048

--- pages/post/game2048.tsx
+++ pages/post/game2048.tsx
@@ -3,19 +3,22 @@ import Layout from '@/components/layout';
 import { DIRECTION, Games } from '@/util/games';
 import React, { Component } from 'react';
 
-export default class game2048 extends Component<{}, { matrix: Array<Array<number>>; games: Games }> {
+export default class game2048 extends Component<{}, { matrix: Array<Array<number>>; games: Games; score: number }> {
   constructor(props: {}) {
     super(props);
     const games = new Games();
-    this.state = { matrix: [], games: games };
+    this.state = { matrix: [], games: games, score: games.score };
   }
+  /**
+   * 不同按键向不同方向移动
+   * @param e
+   */
   handleKeyDown(e: KeyboardEvent) {
     const keyCode = e.keyCode;
     e.stopPropagation();
     switch (keyCode) {
       case 38:
         e.preventDefault();
-        console.log(this);
         this.state.games.move(DIRECTION.UP);
         break;
       case 40:
@@ -34,29 +37,62 @@ export default class game2048 extends Component<{}, { matrix: Array<Array<number
         this.state.games.move(DIRECTION.RIGHT);
         break;
     }
-    this.setState({ matrix: this.state.games.matrix });
+    this.setState({ matrix: this.state.games.matrix, score: this.state.games.score });
+  }
+  /**
+   * 重新开始
+   */
+  handleNewGame() {
+    this.state.games.newGame();
+    this.setState({ matrix: this.state.games.matrix, score: this.state.games.score });
   }
   componentDidMount() {
     this.setState({ matrix: this.state.games.matrix });
+    // 监听键盘事件
     window.addEventListener('keydown', (e) => this.handleKeyDown(e));
   }
   componentWillUnmount() {
     window.removeEventListener('keydown', (e) => this.handleKeyDown(e));
   }
   render() {
-    const { matrix } = this.state;
+    const { matrix,  score } = this.state;
     return (
       <Layout>
-        <section className="text-white">
+        <section className="text-white flex">
+          <div>
+            <div className="text-5xl font-semibold mb-4">2048</div>
+            <div className="mb-4">
+              合并数字，组合成<span>2048</span>
+            </div>
+            <div className="m-auto flex gap-4  mb-4">
+              <div
+                className="flex items-center px-4 rounded-lg bg-[#8f7a66] cursor-pointer"
+                onClick={() => {
+                  this.handleNewGame();
+                }}
+              >
+                New Game
+              </div>
+              <div className="flex flex-col items-center bg-[#bbada0] px-4 rounded-lg">
+                <span>score</span>
+                <span>{score}</span>
+              </div>
+            </div>
+            <div>玩法说明：使用方向键移动方块, 当两个具有相同数字的方块碰触时,它们会和二为一!</div>
+          </div>
+
           <div className={`${styles.gameBox} text-5xl font-semibold p-4 m-auto`}>
             {matrix.map((row, i) =>
               row.map((col, j) => (
                 <div
                   className="flex items-center justify-center  "
-                  style={{ background: 'rgba(238, 228, 218, 0.35)' }}
+                  style={{
+                    background: col > 0 ? `rgba(${(238 * col) % 255}, 228, 218, 1)` : 'rgba(238 , 228, 218, 0.35)',
+                  }}
                   key={i + '' + j}
                 >
-                  {col}
+                  {/* 为0时不显示 */}
+                  {col > 0 ? col : ''}
                 </div>
               )),
             )}
--- util/games.ts
+++ util/games.ts
@@ -16,6 +16,10 @@ export class Games {
         [0, 0, 0, 0],
         [0, 0, 0, 0]
     ]
+    score = 0
+    /**
+     * 将不同移动方向抽离成函数，计算下一个坐标，减少判断
+     */
     #next = {
         [DIRECTION.DOWN]: (i: number, j: number): Array<number> => [i - 1, j],
         [DIRECTION.LEFT]: (i: number, j: number): Array<number> => [i, j + 1],
@@ -23,9 +27,10 @@ export class Games {
         [DIRECTION.RIGHT]: (i: number, j: number): Array<number> => [i, j - 1]
     }
     constructor() {
-        this.#newGame()
+        this.newGame()
     }
-    #newGame() {
+    newGame() {
+        this.score = 0;
         this.matrix = [
             [0, 0, 0, 0],
             [0, 0, 0, 0],
@@ -56,6 +61,12 @@ export class Games {
             this.#randomPos()
         }
     }
+    /**
+     * 判断是否在矩阵内
+     * @param i 纵坐标
+     * @param j 横坐标
+     * @returns 
+     */
     #inRange(i: number, j: number) {
         return this.matrix[i] && this.matrix[i][j] !== undefined
     }
@@ -63,10 +74,12 @@ export class Games {
      * 获取下一个值
      */
     #getNext(direction: DIRECTION, i: number, j: number) {
+        // 根据方向获取下一个坐标
         let [nextI, nextJ] = this.#next[direction](i, j)
         // 判断是否在矩阵内
         while (this.#inRange(nextI, nextJ)) {
             const nextValue = this.matrix[nextI][nextJ]
+            // 如果该坐标不为0 返回结果 为0寻找下一个坐标，知道找到为止
             if (nextValue !== 0) {
                 return [nextI, nextJ, nextValue]
             } else {
@@ -75,21 +88,31 @@ export class Games {
         }
 
     }
+    /**
+     * 计算值
+     * @param direction 方向
+     * @param i 纵坐标
+     * @param j 横坐标
+     * @returns 
+     */
     #calculate(direction: DIRECTION, i: number, j: number) {
         if (!this.#inRange(i, j)) return
         // 计算这个位置的值
         const result = this.#getNext(direction, i, j)
         if (!result) return
         const [nextI, nextJ, nextValue] = result
+        // 判断当前值是否为0 为0则交换值后继续计算
         if (this.matrix[i][j] === 0) {
             this.matrix[i][j] = nextValue
             this.matrix[nextI][nextJ] = 0
             // 为0 交换位置后再计算一次
             this.#calculate(direction, i, j)
+            // 判断当前值和下一个值是否相等 相等累加 
         } else if (this.matrix[i][j] === nextValue) {
             // 和下一个值相等则累加
             this.matrix[i][j] += nextValue
             this.matrix[nextI][nextJ] = 0
+            this.score += 2
         }
         // 计算下个位置的值
         const [_nextI, _nextJ] = this.#next[direction](i, j)
@@ -98,28 +121,33 @@ export class Games {
     move(direction: DIRECTION) {
         switch (direction) {
             case DIRECTION.DOWN:
+                // 向下：从左向右遍历 从最后一行计算
                 for (let j = 0; j < 4; j++) {
                     this.#calculate(direction, 3, j)
                 }
                 break;
             case DIRECTION.UP:
+                // 向上：从左向右遍历列 从第一行开始计算
                 for (let j = 0; j < 4; j++) {
 
                     this.#calculate(direction, 0, j)
                 }
                 break;
             case DIRECTION.LEFT:
+                // 向左：从上往下遍历行 从第一列开始计算
                 for (let i = 0; i < 4; i++) {
                     this.#calculate(direction, i, 0)
                 }
                 break;
             case DIRECTION.RIGHT:
             default:
+                // 向右：从上往下遍历行 从最后一列开始计算
                 for (let i = 0; i < 4; i++) {
                     this.#calculate(direction, i, 3)
                 }
                 break;
         }
+        // 每次移动后新加一个方块
         this.#randomPos()
     }
 
